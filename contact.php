91a6c677a3008afe7cba5cebd8928ba61e02e544
gpr4do <gpr4do@gmail.com>
2022-05-04 15:37:18 -0300

    separeted footer and header archives

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
   include_once("templates/header.php")
 ?>
 
-  <h1>Ola Mundo</h1>
+  <h1>Pagina de Contato</h1>
 
 <?php
   include_once("templates/footer.php")
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>The Bear Tech &copy; 2022</p>
+</footer>
+
+</body>
+</html>
